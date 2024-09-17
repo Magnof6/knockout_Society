cc165bfe7f30bc6d9906a832223d96066f1dc055
GiulioTizzano <115190185+GiulioTizzano@users.noreply.github.com>
2024-09-17 19:16:04 +0200

    Add files via upload

--- ImageToText.py
+++ ImageToText.py
@@ -3,3 +3,13 @@ Esto que encapsule obtener el texto de una imagen. Puede que lo usemos en muchas
 Encargado:
 
 '''
+
+import pytesseract as tess
+
+tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+from PIL import Image
+
+img = Image.open('media\imagen_texto.png')
+
+text = tess.image_to_string(img)
+print(text)
