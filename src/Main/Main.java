86ef2d5eeea62421bdbfff8818d9ca4f453f16fc
AlexRF14 <alejandro.rodriguezferrer@usp.ceu.es>
2024-09-30 13:15:51 +0200

    Creación de un main

---
+++ src/Main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+
+import Funciones.Funciones;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Funciones funciones = new Funciones();
+        
+    }
+}
