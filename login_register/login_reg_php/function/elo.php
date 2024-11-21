f2d168030a764392b3713f688f4de1b46ba55922
AlexRF14 <alejandro.rodriguezferrer@usp.ceu.es>
2024-11-21 10:31:47 +0100

    Elo v2 falta bbdd

--- login_register/login_reg_php/function/elo.php
+++ login_register/login_reg_php/function/elo.php
@@ -28,6 +28,11 @@
  * 
  */
 class Elo{
+    public $conn;
+
+    public function __construct($dbConnection){
+        $this->conn = $dbConnection;
+    }
     public function calculaNuevoElo($R_a , $R_b, $S_a , $k = 40){
         $E_a = 1 / (1 + 10 ** (($R_b - $R_a)/400));
         $E_b = 1 - $E_a;
@@ -37,5 +42,45 @@ class Elo{
         $nuevo_elo_b = $R_b + $k * ($S_b - $E_b);
 
         return [$nuevo_elo_a , $nuevo_elo_b];
+    }
+    public function extraerEloLuchadoresBefore($email_Luchador_1 , $email_Luchador_2){
+        $sql = "SELECT puntos FROM usuario WHERE email = :email_Luchador_1";
+        $stmt = $this->conn->prepare($sql);
+        $stmt -> bindParam(':email_Luchador_1' , $email_Luchador_1, PDO::PARAM_STR);
+        $stmt->execute();
+        $puntosL_1 = $stmt->fetch(PDO::FETCH_ASSOC);
+        $puntosL_1 = $puntosL_1['puntos'];
+
+        $sql = "SELECT puntos FROM usuario WHERE email = :email_Luchador_2";
+        $stmt = $this->conn->prepare($sql);
+        $stmt -> bindParam(':email_Luchador_2' , $email_Luchador_2, PDO::PARAM_STR);
+        $stmt->execute();
+        $puntosL_2 = $stmt->fetch(PDO::FETCH_ASSOC);
+        $puntosL_2 = $puntosL_2['puntos'];
+
+        $puntos = [$puntosL_1 , $puntosL_2];
+
+        return $puntos;
+
+    }
+
+    public function actualizarEloLuchadores($id_lucha , $email_Luchador_1 , $email_Luchador_2, $resultado){
+
+        $puntos = $this -> extraerEloLuchadoresBefore($email_Luchador_1 , $email_Luchador_2);
+        if($puntos){
+            $R_a = $puntos[0];
+            $R_b = $puntos[1];
+            $puntos_f = $this->calculaNuevoElo($R_a , $R_b, $resultado, $k = 40);
+        }
+        if($puntos_f){
+            $puntosLn_1 = $puntos_f[0];
+            $puntosLn_2 = $puntos_f[1];
+            
+        }
+
+        
+
+
+
     }
 }
